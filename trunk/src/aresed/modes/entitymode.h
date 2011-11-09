1feb1c00c21baaec5da612791b7b8f25ff561abf
jorrit.tyberghein@gmail.com <jorrit.tyberghein@gmail.com@6bb8ba83-2c51-affa-89f7-aaaa9c63d2cd>
2011-11-09 10:09:17 +0000

    Added a new (and empty/non-functional) entity editing mode.

---
+++ trunk/src/aresed/modes/entitymode.h
@@ -0,0 +1,68 @@
+/*
+The MIT License
+
+Copyright (c) 2011 by Jorrit Tyberghein
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+THE SOFTWARE.
+ */
+
+#ifndef __aresed_entitymode_h
+#define __aresed_entitymode_h
+
+#include "csutil/csstring.h"
+#include "editmodes.h"
+
+struct iGeometryGenerator;
+
+class EntityMode : public EditingMode
+{
+private:
+
+public:
+  EntityMode (wxWindow* parent, AresEdit3DView* aresed3d);
+  virtual ~EntityMode () { }
+
+  virtual void Start ();
+  virtual void Stop ();
+
+  virtual void FramePre();
+  virtual void Frame3D();
+  virtual void Frame2D();
+  virtual bool OnKeyboard(iEvent& ev, utf32_char code);
+  virtual bool OnMouseDown(iEvent& ev, uint but, int mouseX, int mouseY);
+  virtual bool OnMouseUp(iEvent& ev, uint but, int mouseX, int mouseY);
+  virtual bool OnMouseMove(iEvent& ev, int mouseX, int mouseY);
+
+  class Panel : public wxPanel
+  {
+  public:
+    Panel(wxWindow* parent, EntityMode* s)
+      : wxPanel (parent, wxID_ANY, wxDefaultPosition, wxDefaultSize), s (s)
+    {}
+
+  private:
+    EntityMode* s;
+
+    DECLARE_EVENT_TABLE()
+  };
+  Panel* panel;
+};
+
+#endif // __aresed_entitymode_h
+
