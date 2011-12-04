98319d78d64f08273343ce3bfb0d7b6e3842db86
jorrit.tyberghein@gmail.com <jorrit.tyberghein@gmail.com@6bb8ba83-2c51-affa-89f7-aaaa9c63d2cd>
2011-12-04 14:39:18 +0000

    Added GUI for the new dynamic factory editor. Currently not functional.

---
+++ trunk/src/aresed/ui/dynfactdialog.cpp
@@ -0,0 +1,65 @@
+/*
+The MIT License
+
+Copyright (c) 2011 by Jorrit Tyberghein
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+THE SOFTWARE.
+ */
+
+#include "../apparesed.h"
+#include "dynfactdialog.h"
+#include "uimanager.h"
+#include "listctrltools.h"
+
+//--------------------------------------------------------------------------
+
+BEGIN_EVENT_TABLE(DynfactDialog, wxDialog)
+  EVT_BUTTON (XRCID("okButton"), DynfactDialog :: OnOkButton)
+END_EVENT_TABLE()
+
+//--------------------------------------------------------------------------
+
+void DynfactDialog::OnOkButton (wxCommandEvent& event)
+{
+  EndModal (TRUE);
+}
+
+void DynfactDialog::UpdateTree ()
+{
+  wxTreeCtrl* list = XRCCTRL (*this, "factoryTree", wxTreeCtrl);
+}
+
+void DynfactDialog::Show ()
+{
+  selIndex = -1;
+  UpdateTree ();
+  ShowModal ();
+}
+
+DynfactDialog::DynfactDialog (wxWindow* parent, UIManager* uiManager) :
+  uiManager (uiManager)
+{
+  wxXmlResource::Get()->LoadDialog (this, parent, wxT ("DynfactDialog"));
+}
+
+DynfactDialog::~DynfactDialog ()
+{
+}
+
+
