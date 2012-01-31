874e9ec4566dc70075a7690205e27c2256adc76d
jorrit.tyberghein@gmail.com <jorrit.tyberghein@gmail.com@6bb8ba83-2c51-affa-89f7-aaaa9c63d2cd>
2012-01-31 09:09:26 +0000

    Added support for editing pivot joints in the dynamic factory editor.

--- trunk/src/aresed/ui/dynfactdialog.cpp
+++ trunk/src/aresed/ui/dynfactdialog.cpp
@@ -65,10 +65,10 @@ void DynfactMeshView::SyncValue (Ares::Value* value)
 void DynfactMeshView::SetupColliderGeometry ()
 {
   ClearGeometry ();
-  long idx = dialog->GetSelectedCollider ();
   iDynamicFactory* fact = dialog->GetCurrentFactory ();
   if (fact)
   {
+    long idx = dialog->GetSelectedCollider ();
     for (size_t i = 0 ; i < fact->GetBodyCount () ; i++)
     {
       size_t pen = i == size_t (idx) ? hilightPen : normalPen;
@@ -82,12 +82,132 @@ void DynfactMeshView::SetupColliderGeometry ()
       else if (info.type == BODY_MESH || info.type == BODY_CONVEXMESH)
 	AddMesh (info.offset, pen);
     }
+    idx = dialog->GetSelectedPivot ();
+    for (size_t i = 0 ; i < fact->GetPivotJointCount () ; i++)
+    {
+      size_t pen = i == size_t (idx) ? hilightPen : normalPen;
+      csVector3 pos = fact->GetPivotJointPosition (i);
+      AddSphere (pos, .01, pen);
+    }
   }
 }
 
 //--------------------------------------------------------------------------
 
 using namespace Ares;
+
+/**
+ * A composite value representing a pivot joint for a dynamic factory.
+ */
+class PivotValue : public CompositeValue
+{
+private:
+  size_t idx;
+  iDynamicFactory* dynfact;
+  csVector3 pos;
+
+protected:
+  virtual void ChildChanged (Value* child)
+  {
+    dynfact->SetPivotJointPosition (idx, pos);
+    FireValueChanged ();
+  }
+
+public:
+  PivotValue (size_t idx, iDynamicFactory* dynfact) : idx (idx), dynfact (dynfact)
+  {
+    if (dynfact) pos = dynfact->GetPivotJointPosition (idx);
+    AddChild ("pivotX", NEWREF(Value,new FloatPointerValue (&pos.x)));
+    AddChild ("pivotY", NEWREF(Value,new FloatPointerValue (&pos.y)));
+    AddChild ("pivotZ", NEWREF(Value,new FloatPointerValue (&pos.z)));
+  }
+  virtual ~PivotValue () { }
+
+  virtual csString Dump (bool verbose = false)
+  {
+    csString dump = "[Piv]";
+    dump += CompositeValue::Dump (verbose);
+    return dump;
+  }
+};
+
+/**
+ * A value representing the list of pivot joints for a dynamic factory.
+ * Children of this value are of type PivotValue.
+ */
+class PivotCollectionValue : public StandardCollectionValue
+{
+private:
+  DynfactDialog* dialog;
+  iDynamicFactory* dynfact;
+
+  // Create a new child and add to the array.
+  Value* NewChild (size_t i)
+  {
+    csRef<PivotValue> value;
+    value.AttachNew (new PivotValue (i, dynfact));
+    children.Push (value);
+    value->SetParent (this);
+    return value;
+  }
+
+protected:
+  virtual void UpdateChildren ()
+  {
+    if (dynfact != dialog->GetCurrentFactory ()) dirty = true;
+    if (!dirty) return;
+    ReleaseChildren ();
+    dynfact = dialog->GetCurrentFactory ();
+    if (!dynfact) return;
+    dirty = false;
+    for (size_t i = 0 ; i < dynfact->GetPivotJointCount () ; i++)
+      NewChild (i);
+  }
+  virtual void ChildChanged (Value* child)
+  {
+    FireValueChanged ();
+    //UIManager* uiManager = dialog->GetUIManager ();
+    //AresEdit3DView* ares3d = uiManager->GetApp ()->GetAresView ();
+    //ares3d->SetupFactorySettings (dialog->GetCurrentFactory ());
+  }
+
+public:
+  PivotCollectionValue (DynfactDialog* dialog) : dialog (dialog), dynfact (0) { }
+  virtual ~PivotCollectionValue () { }
+
+  virtual bool DeleteValue (Value* child)
+  {
+    dynfact = dialog->GetCurrentFactory ();
+    if (!dynfact) return false;
+    for (size_t i = 0 ; i < children.GetSize () ; i++)
+      if (children[i] == child)
+      {
+	dynfact->RemovePivotJoint (i);
+	child->SetParent (0);
+	children.DeleteIndex (i);
+	FireValueChanged ();
+	return true;
+      }
+    return false;
+  }
+  virtual Value* NewValue (size_t idx, Value* selectedValue, const DialogResult& suggestion)
+  {
+    dynfact = dialog->GetCurrentFactory ();
+    if (!dynfact) return 0;
+    dynfact->CreatePivotJoint (csVector3 (0, 0, 0));
+    idx = dynfact->GetPivotJointCount ()-1;
+    Value* value = NewChild (idx);
+    FireValueChanged ();
+    return value;
+  }
+
+  virtual csString Dump (bool verbose = false)
+  {
+    csString dump = "[Piv*]";
+    dump += StandardCollectionValue::Dump (verbose);
+    return dump;
+  }
+};
 
 /**
  * A value for the type of a collider.
@@ -182,17 +302,16 @@ public:
   ColliderValue (size_t idx, iDynamicFactory* dynfact) : idx (idx), dynfact (dynfact)
   {
     if (dynfact) info = dynfact->GetBody (idx);
-    csRef<Value> v;
-    v.AttachNew (new ColliderTypeValue (&ColliderValue::info.type)); AddChild ("type", v);
-    v.AttachNew (new FloatPointerValue (&ColliderValue::info.offset.x)); AddChild ("offsetX", v);
-    v.AttachNew (new FloatPointerValue (&ColliderValue::info.offset.y)); AddChild ("offsetY", v);
-    v.AttachNew (new FloatPointerValue (&ColliderValue::info.offset.z)); AddChild ("offsetZ", v);
-    v.AttachNew (new FloatPointerValue (&ColliderValue::info.mass)); AddChild ("mass", v);
-    v.AttachNew (new FloatPointerValue (&ColliderValue::info.radius)); AddChild ("radius", v);
-    v.AttachNew (new FloatPointerValue (&ColliderValue::info.length)); AddChild ("length", v);
-    v.AttachNew (new FloatPointerValue (&ColliderValue::info.size.x)); AddChild ("sizeX", v);
-    v.AttachNew (new FloatPointerValue (&ColliderValue::info.size.y)); AddChild ("sizeY", v);
-    v.AttachNew (new FloatPointerValue (&ColliderValue::info.size.z)); AddChild ("sizeZ", v);
+    AddChild ("type", NEWREF(Value,new ColliderTypeValue (&ColliderValue::info.type)));
+    AddChild ("offsetX", NEWREF(Value,new FloatPointerValue (&ColliderValue::info.offset.x)));
+    AddChild ("offsetY", NEWREF(Value,new FloatPointerValue (&ColliderValue::info.offset.y)));
+    AddChild ("offsetZ", NEWREF(Value,new FloatPointerValue (&ColliderValue::info.offset.z)));
+    AddChild ("mass", NEWREF(Value,new FloatPointerValue (&ColliderValue::info.mass)));
+    AddChild ("radius", NEWREF(Value,new FloatPointerValue (&ColliderValue::info.radius)));
+    AddChild ("length", NEWREF(Value,new FloatPointerValue (&ColliderValue::info.length)));
+    AddChild ("sizeX", NEWREF(Value,new FloatPointerValue (&ColliderValue::info.size.x)));
+    AddChild ("sizeY", NEWREF(Value,new FloatPointerValue (&ColliderValue::info.size.y)));
+    AddChild ("sizeZ", NEWREF(Value,new FloatPointerValue (&ColliderValue::info.size.z)));
   }
   virtual ~ColliderValue () { }
 
@@ -375,6 +494,7 @@ DynfactValue::DynfactValue (DynfactDialog* dialog) : dialog (dialog)
 {
   // Setup the composite representing the dynamic factory that is selected.
   AddChild ("colliders", NEWREF(Value,new ColliderCollectionValue (dialog)));
+  AddChild ("pivots", NEWREF(Value,new PivotCollectionValue (dialog)));
   AddChild ("maxRadius", NEWREF(Value,new MaxRadiusValue(dialog)));
   AddChild ("imposterRadius", NEWREF(Value,new ImposterRadiusValue(dialog)));
   AddChild ("static", NEWREF(Value,new StaticValue(dialog)));
@@ -556,6 +676,12 @@ long DynfactDialog::GetSelectedCollider ()
   return ListCtrlTools::GetFirstSelectedRow (list);
 }
 
+long DynfactDialog::GetSelectedPivot ()
+{
+  wxListCtrl* list = XRCCTRL (*this, "pivots_List", wxListCtrl);
+  return ListCtrlTools::GetFirstSelectedRow (list);
+}
+
 DynfactDialog::DynfactDialog (wxWindow* parent, UIManager* uiManager) :
   View (this), uiManager (uiManager)
 {
@@ -579,23 +705,33 @@ DynfactDialog::DynfactDialog (wxWindow* parent, UIManager* uiManager) :
   wxTreeCtrl* factoryTree = XRCCTRL (*this, "factoryTree", wxTreeCtrl);
   factorySelectedValue.AttachNew (new TreeSelectedValue (factoryTree, dynfactCollectionValue, VALUE_COLLECTION));
 
-  // Setup the collider list.
+  // Setup the collider and pivot list.
   DefineHeading ("colliders_List", "Type,Mass,x,y,z", "type,mass,offsetX,offsetY,offsetZ");
+  DefineHeading ("pivots_List", "x,y,z", "pivotX,pivotY,pivotZ");
 
   // Setup the composite representing the dynamic factory that is selected.
   dynfactValue.AttachNew (new DynfactValue (this));
   Bind (dynfactValue, this);
-  Value* colliders = dynfactValue->GetChildByName ("colliders");
 
   // Create a selection value that will follow the selection on the collider list.
+  Value* colliders = dynfactValue->GetChildByName ("colliders");
   wxListCtrl* colliderList = XRCCTRL (*this, "colliders_List", wxListCtrl);
   colliderSelectedValue.AttachNew (new ListSelectedValue (colliderList, colliders, VALUE_COMPOSITE));
   colliderSelectedValue->SetupComposite (NEWREF(Value,new ColliderValue(0,0)));
+
+  // Create a selection value that will follow the selection on the pivot list.
+  Value* pivots = dynfactValue->GetChildByName ("pivots");
+  wxListCtrl* pivotsList = XRCCTRL (*this, "pivots_List", wxListCtrl);
+  csRef<ListSelectedValue> pivotsSelectedValue;
+  pivotsSelectedValue.AttachNew (new ListSelectedValue (pivotsList, pivots, VALUE_COMPOSITE));
+  pivotsSelectedValue->SetupComposite (NEWREF(Value,new PivotValue(0,0)));
 
   // Bind the selected collider value to the mesh view. This value is not actually
   // used by the mesh view but this binding only serves as a signal for the mesh
   // view to update itself.
   Bind (colliderSelectedValue, meshView);
+  // Also do this for the selected pivot value.
+  Bind (pivotsSelectedValue, meshView);
 
   // Connect the selected value from the catetory tree to the dynamic
   // factory value so that the two radius values and the collider list
@@ -603,9 +739,10 @@ DynfactDialog::DynfactDialog (wxWindow* parent, UIManager* uiManager) :
   // with 'dochildren' equal to true to make sure the children get notified
   // as well (i.e. the list for example).
   Signal (factorySelectedValue, dynfactValue, true);
-  // Also connect it to the selected value so that a new mesh is rendered
+  // Also connect it to the selected values so that a new mesh is rendered
   // when the current dynfact changes.
   Signal (factorySelectedValue, colliderSelectedValue);
+  Signal (factorySelectedValue, pivotsSelectedValue);
 
   // Bind the selection value to the different panels that describe the different types of colliders.
   Bind (colliderSelectedValue->GetChildByName ("type"), "type_colliderChoice");
@@ -615,9 +752,13 @@ DynfactDialog::DynfactDialog (wxWindow* parent, UIManager* uiManager) :
   Bind (colliderSelectedValue, "mesh_ColliderPanel");
   Bind (colliderSelectedValue, "convexMesh_ColliderPanel");
 
+  Bind (pivotsSelectedValue, "pivotPosition_Panel");
+
   // The actions.
   AddAction (colliderList, NEWREF(Action, new NewChildAction (colliders)));
   AddAction (colliderList, NEWREF(Action, new DeleteChildAction (colliders)));
+  AddAction (pivotsList, NEWREF(Action, new NewChildAction (pivots)));
+  AddAction (pivotsList, NEWREF(Action, new DeleteChildAction (pivots)));
   AddAction (factoryTree, NEWREF(Action, new NewChildDialogAction (dynfactCollectionValue, factoryDialog)));
   AddAction (factoryTree, NEWREF(Action, new DeleteChildAction (dynfactCollectionValue)));
   AddAction (factoryTree, NEWREF(Action, new EditCategoryAction (this)));
