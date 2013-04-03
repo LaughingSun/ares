901d2c6b2ed834ca3f023d328eb69b193f8caef9
jorrit.tyberghein@gmail.com <jorrit.tyberghein@gmail.com@6bb8ba83-2c51-affa-89f7-aaaa9c63d2cd>
2013-04-03 09:46:33 +0000

    Editing the tag of a property class no longer clears all the parameters and default state.

--- trunk/src/editorplugins/entitymode/pcpanel.h
+++ trunk/src/editorplugins/entitymode/pcpanel.h
@@ -70,6 +70,7 @@ private:
 
   void OnChoicebookPageChange (wxChoicebookEvent& event);
   void OnUpdateEvent (wxCommandEvent& event);
+  void OnUpdateTag (wxCommandEvent& event);
 
   /**
    * Possibly switch the type of the PC. Do nothing if the PC is
